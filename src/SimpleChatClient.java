bf0c6e58cd15dfb1ea9e1200a6bd042d7f4dc9a7
rm7740 <adavid8a@hotmail.com>
2016-02-07 15:26:27 -0500

    Add the go method that creates the GUI ans starts the app

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -1,5 +1,58 @@
-/**
- * Created by Dell on 7/2/2016.
- */
+import org.omg.PortableInterceptor.INACTIVE;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class SimpleChatClient {
+    JTextArea incoming;
+    JTextField outgoing;
+    BufferedReader reader;
+    PrintWriter writer;
+    Socket sock;
+
+    public static void main(String[] args) {
+        SimpleChatClient client = new SimpleChatClient();
+        client.go();
+    }
+
+    private void go() {
+        JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+        JPanel mainpanel = new JPanel();
+        incoming = new JTextArea(15, 50);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(20);
+        JButton sendbutton = new JButton("Send");
+        sendbutton.addActionListener(new SendButtonListener());
+        mainpanel.add(qScroller);
+        mainpanel.add(outgoing);
+        mainpanel.add(sendbutton);
+        setUpNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
+        frame.getContentPane().add(BorderLayout.CENTER, mainpanel):
+        frame.setSize(400, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private void setUpNetworking() {
+    }
+
+    private class SendButtonListener implements ActionListener {
+    }
+
+    private class IncomingReader implements Runnable {
+    }
 }
+
