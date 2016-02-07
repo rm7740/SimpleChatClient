bdfdd45d7483b1f5139ae3bde617b86654dc5507
rm7740 <adavid8a@hotmail.com>
2016-02-07 15:39:47 -0500

    Add the IncomingReader inner class that starts a new thread

--- src/SimpleChatClient.java
+++ src/SimpleChatClient.java
@@ -75,6 +75,17 @@ public class SimpleChatClient {
     }
 
     private class IncomingReader implements Runnable {
+        public void run(){
+            String message;
+            try{
+                while((message = reader.readLine()) != null){
+                    System.out.println("read" + message);
+                    incoming.append(message + "\n");
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
 
