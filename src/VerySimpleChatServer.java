d237ba17d5e2748ab974de3f4baabc17fb5ebc7b
rm7740 <adavid8a@hotmail.com>
2016-02-07 16:07:23 -0500

    Add the go method on the server

--- src/VerySimpleChatServer.java
+++ src/VerySimpleChatServer.java
@@ -1,10 +1,35 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class VerySimpleChatServer {
     ArrayList clientOutputStreams;
+
+    public static void main(String[] args) {
+        new VerySimpleChatServer().go();
+    }
+
+    private void go() {
+        clientOutputStreams = new ArrayList();
+        try{
+            ServerSocket serverSock = new ServerSocket(5000);
+
+            while (true){
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+                System.out.println("Got a connection");
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 
     public class ClientHandler implements Runnable{
         BufferedReader reader;
