6154a8cbd932b12f932da00736a3a09a422bca89
rm7740 <adavid8a@hotmail.com>
2016-02-07 16:00:05 -0500

    Add the ClientHandler inner class in the server

--- src/VerySimpleChatServer.java
+++ src/VerySimpleChatServer.java
@@ -1,6 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.ArrayList;
 
-public class VerySimpleCahtServer {
+public class VerySimpleChatServer {
     ArrayList clientOutputStreams;
 
+    public class ClientHandler implements Runnable{
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket){
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
+        public void run(){
+            String message;
+            try{
+                while ((message = reader.readLine()) != null){
+                    System.out.println("read" + message);
+                    tellEveryone(message);
+                }
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void tellEveryone(String message) {
+    }
 }
